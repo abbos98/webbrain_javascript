b38fc87f701e953755ebd2c6c47f0cedb6e61268
Abbos <abbos.pankiy2018@gmail.com>
2024-06-05 02:09:41 +0900

    eighth lesson

---
+++ lesson8/script.js
@@ -0,0 +1,124 @@
+// <<<<<<< Math build in methods >>>>>>>>>
+
+// console.log(Math.pow(3, 2));
+// console.log(3 ** 2);
+
+// => darajaga kotarib beradi!
+////////////////////////////////////////
+
+// console.log(Math.sqrt(64));
+// console.log(64 ** 0.5);
+////////////////////////////////////////
+
+// console.log(Math.cbrt(64));
+// 4 * 4 * 4 = 64;
+////////////////////////////////////////
+
+// console.log(Math.min(20, 10, 5));
+// console.log(Math.max(20, 10, 5));
+////////////////////////////////////////
+
+// console.log(Math.abs(-10));
+////////////////////////////////////////
+
+// console.log(Number.parseInt(Math.random() * 10));
+
+
+// => Math.round(10.5);
+// (10.5) bolsa 11 ciqarib beradi / (10.4) 10ga teng boladi
+
+// console.log(Math.round(10.5));
+// console.log(Math.round(10.4));
+
+// => Math.floor
+
+//console.log(Math.floor(10.5));
+
+// console.log(Math.ceil(10.1));
+
+
+
+// <<<<<<<<< Funtion (code of block) >>>>>>>>
+
+// Decleration, Expression, Arrow
+
+
+
+// Declaration function
+
+// function example() {
+//     console.log('WBA');
+// }
+
+// example();    => calling function
+
+
+// Expression function 
+
+// let example = function() {
+//     console.log('WBA');
+// }
+
+// example();
+
+
+// Arrow function 
+
+// let example = () => {
+//     console.log('Wba');
+// }
+
+// example();
+
+
+// <<<<<<<<<< Return >>>>>>>>>
+
+// return in declaration function
+
+// function example () {
+//     return 'WBA'
+// }
+
+// console.log(example());
+
+
+
+// return in expression function
+
+//let example = function () {
+  //  return 5 + 5
+//}
+//console.log(example());
+
+
+// return in arrow function 
+
+//let example = () => {
+//  return 10 + 10
+//}
+//console.log(example());
+
+// Avto return 
+
+//let example = () => 10 + 10;
+
+//console.log(example());
+
+
+
+// <<<<<< parametr vs argument >>>>>>>
+
+//let example = (name, age, status) => {
+//  console.log(`name: ${name} Age: ${age} Status: ${status}`);
+//}
+//example('Abbos', 25, 'Student');
+//example('Madina', 23, 'Student');
+
+
+
+// Default parament
+
+//let example = (name = 'Abbos', age = 20, status = 'Student') => {
+//  console.log(`Name: ${name} Age: ${age} Status: ${status}`);
+//}
+//example()
