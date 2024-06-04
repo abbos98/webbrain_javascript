8d31fa0f9d5e2a5a51bc075e73d0d154483ba6dc
Abbos <abbos.pankiy2018@gmail.com>
2024-06-04 21:06:56 +0900

    7-hometask

---
+++ lesson7/hometask.js
@@ -0,0 +1,66 @@
+// <<<< 1 >>>>
+
+// let str = 'foziL    ';
+
+// // console.log(str.trim().toLowerCase().endsWith('l'));
+
+// let newStr = str.trim().toLowerCase();
+// // let result =newStr[newStr.length - 1] === 'l';
+
+// console.log(result);
+
+
+// <<<<< 2 >>>>>>
+// qiymatlarni ozgartirish!
+
+// let str1 = 'Web';
+// let str2 = 'Brain';
+// let newStr = str1;
+
+// str1 = str2;
+// str2 = newStr;
+
+// console.log(str1);
+// console.log(str2);
+
+///////////////////////////////////////////////
+
+// <<<<<< 3 >>>>>>>
+
+// let yil = 2025;
+
+// yil % 4 === 0 && console.log('Kabisa yili');
+// yil % 4 !== 0 && console.log('Kabisa yili emas');
+
+////////////////////////////////////////////////
+
+// <<<<<< 4 >>>>>>
+
+// let str = 'Web Brain';
+
+// let newStr = str.replace(/Web/g, 'IT').repeat(10);
+
+
+
+// let title = 'Abbosbek';
+
+
+
+// let to = title.match(/[a-z]/g);
+// let toString = to.join("");
+// console.log(typeof toString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
