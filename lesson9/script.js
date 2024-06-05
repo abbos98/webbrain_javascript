c5742e6faceab8fd72a4738891c9ff62cc7e2f4f
Abbos <abbos.pankiy2018@gmail.com>
2024-06-06 05:15:15 +0900

    ninth lesson

---
+++ lesson9/script.js
@@ -0,0 +1,99 @@
+// Object 
+
+
+// let user = {
+//     name:'Abbos',
+//     age: 20
+// }
+
+
+// Accessing to object value 
+
+// <<<<<<< 1 >>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+//     'Full-name': 'Begmatov Abbos Abduzokhid Ugli',
+//     100: 2200,
+// }
+//<<<<<<<<<< 1-usul >>>>>>>>>>
+
+// console.log(student.name);
+// console.log(student.age);                  
+// console.log(student.Full-name);     
+// console.log(student.100);           
+
+// <<<<<<<<< 2-usul >>>>>>>>>>
+// console.log(student['name']);
+// console.log(student['age']);              
+// console.log(student['Full-name']);
+// console.log(student[100]);
+
+
+// <<<<<<<<< 3-usul (dynamic access) >>>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+// }
+
+// let key = 'name';
+
+// console.log(student[key]);
+
+
+//////////////////////////////////////////////////////////////////////////
+
+// Object update, delete, create
+
+// let student = {
+//         name: 'Madina',
+//         age:20,
+//         status: 'Student',
+//     }
+
+//     // Object.freeze(student);
+//     // Object.seal(student);
+
+
+//    student.name = 'Abbosbek';       
+//    student.status = 'Student';       
+//    delete student.age
+
+//    console.log(student);
+
+//////////////////////////////////////////////////////////////
+
+// <<<<<< Object methods >>>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+// }
+
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+// console.log(Object.entries(student));
+
+/////////////////////////////////////////////////////////////
+
+// <<<<<<< For in loop >>>>>>>>
+
+// let student = {
+//         name: 'Abbos',
+//         age:20,
+//     }
+    
+//     for(let key in student) {
+//         // console.log(key);
+//         // console.log(student[key]);
+//     }
+
+
+
+
+
+
+
+
