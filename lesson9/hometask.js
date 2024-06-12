0490df029aa3b9ac7aa88dc8c3081e3e4d85346c
Abbos <abbos.pankiy2018@gmail.com>
2024-06-13 00:25:45 +0900

    ninth lesson

--- lesson9/hometask.js
+++ lesson9/hometask.js
@@ -67,6 +67,21 @@
 // multiplyNumeric(menu)
 // console.log(menu);
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// <<<<<< agar qiymat bolsa false bomlasa true qaytardi >>>>>>>>>
+
+// let schedule = {};
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false
+//     }
+//     return true
+
+// }
+
+// console.log(isEmpty(schedule));
 
 
 
